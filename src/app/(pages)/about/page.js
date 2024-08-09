289700934d260d87ce31a64dba281fa3997b45ca
farxodnazarov <farhodnazarov002gmail.com>
2024-08-09 11:09:38 +0500

    finished

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -1,7 +1,7 @@
 import Form from "@/components/Form";
 import ShowBooks from "@/components/ShowBooks";
 
-
+ 
 const getData = async () => {
   const req = await fetch(
     "https://online-json-server-api.up.railway.app/project/66b4546700b1a8ec3af1d68d/books",
